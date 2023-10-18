48673f1171a5a196e430f7d3d0151082228f613d
TiboHrqn <harquin.thibault@gmail.com>
2023-10-18 14:29:51 +0200

    statemanager

---
+++ Assets/Scripts/Singletons/StateManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateManager : MonoBehaviour
+{
+    // SINGLETON PART
+    private static StateManager instance = null;
+    public static StateManager Instance => instance;
+
+    //STATE PART
+    private int currentPhase;
+    [SerializeField] private int maxPhase;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);  
+        currentPhase = 0;  
+    }
+
+    public void AchievePhase(int i)
+    {
+        if (currentPhase == i)
+        {
+            Invoke("ToDoAtEndPhase"+currentPhase, 0);
+            if (currentPhase < maxPhase)
+            {
+                currentPhase ++;
+
+            }
+            else
+            {
+                Debug.Log("dernière phase du jeu : " + currentPhase);
+            }
+        }
+    }
+
+    private void ToDoAtEndPhase0()
+    {
+        Debug.Log("fin de la phase 0");
+    }
+
+    private void ToDoAtEndPhase1()
+    {
+        Debug.Log("fin de la phase 1");
+    }
+
+    private void ToDoAtEndPhase2()
+    {
+        Debug.Log("fin de la phase 2");
+    }
+
+
+
+
+
+}
