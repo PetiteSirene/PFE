23bb30cffaad660a1d4ead53600d6449bc769479
TiboHrqn <harquin.thibault@gmail.com>
2023-12-07 18:44:47 +0100

    ball

---
+++ Assets/Scripts/TheVRObject/VRObjectBall.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRObjectBall : MonoBehaviour
+{
+    [SerializeField] private float minHeight;
+    private int labyPhase = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        TryWinLaby();
+    }
+
+    void TryWinLaby()
+    {
+        if (StateManager.Instance.currentPhase == labyPhase && gameObject.transform.position.y < minHeight)
+        {
+            StateManager.Instance.AchievePhase(labyPhase);
+            Destroy(this);
+        }
+    }
+}
