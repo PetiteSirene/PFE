3704925938ae67c394d20c22b729e91b22bad3b4
Ariel <ariel.lissak@gmail.com>
2024-01-25 16:19:00 +0100

    Face laby

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    #region Singleton
+    // SINGLETON PART
     public static SoundManager Instance
     {
         get
@@ -20,7 +20,7 @@ public class SoundManager : MonoBehaviour
         }
     }
     private static SoundManager instance;
-    #endregion
+    //
 
     public AudioSource[] Jingles;
 
